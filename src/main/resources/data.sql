a37e91c3c98b6be65e4bdd79f21998b4a0ed1593
Chandravadan <s.chandravadan@gmail.com>
2017-04-06 01:20:58 +0530

    Seed data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO shops (shop_name, address, latitude, longitude, post_code)
+VALUES ('kharadi-shop','EON IT PARK, Kharadi, Pune',18.5515,73.9508, 411014);
+
+INSERT INTO shops (shop_name, address, latitude, longitude, post_code)
+VALUES ('hinjewadi-shop','Hinjewadi Tech Park, Hinjewadi, Pune',18.5917,73.6838, 411054);
+
+INSERT INTO shops (shop_name, address, latitude, longitude, post_code)
+VALUES ('bengaluru-shop','Electronic city, Bengaluru',12.8399,77.6770, 560100);
+
